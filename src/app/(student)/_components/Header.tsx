0d36e106ac753f4abe66aec8cd322f7506509c62
Ayomide Oluniyi <ayorg360@gmail.com>
2024-11-28 00:22:37 +0200

    chore: update active nav link logic

--- src/app/(student)/_components/Header.tsx
+++ src/app/(student)/_components/Header.tsx
@@ -17,6 +17,7 @@ export default function Header({ routes }: HeaderProps) {
   const user = useAppStore((state) => state.user);
 
   const pathname = usePathname();
+  const rootPath = `/${pathname.split('/')[1]}`
 
   const greeting = useMemo(() => {
     const date = new Date();
@@ -30,7 +31,7 @@ export default function Header({ routes }: HeaderProps) {
     <div className='space-y-6 border-b border-[#B0CAFF1A] px-4 pt-6 sticky top-0 bg-background z-10'>
       <div className='w-full flex items-center justify-between'>
         <div className='flex items-center gap-4'>
-          <Avatar alt={user?.name || ''} size={40} />
+          <Avatar alt={user?.name || ''} size={40} type='square' />
           <div className='flex flex-col'>
             <p className='font-semibold'>Good {greeting} {user?.firstName}</p>
             <Link href={appRoutes.profile} className='text-sm hover:text-primary-100'>View Profile</Link>
@@ -44,8 +45,8 @@ export default function Header({ routes }: HeaderProps) {
         {routes.map((route) => (
           <Link href={route.href} key={route.name}>
             <div className='flex flex-col gap-3 group'>
-              <p className={clsx(pathname == route.href && 'font-semibold text-white', 'text-accent font-medium px-2')}>{route.name}</p>
-              <div className={clsx(pathname == route.href && 'bg-primary', 'w-full h-0.5 group-hover:bg-primary/50')} />
+              <p className={clsx(rootPath === route.href && 'font-semibold text-white', 'text-accent font-medium px-2')}>{route.name}</p>
+              <div className={clsx(rootPath === route.href && 'bg-primary', 'w-full h-0.5 group-hover:bg-primary/50')} />
             </div>
           </Link>
         ))}
