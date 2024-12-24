3189c870807f2da719a4adec9b2064ad9fa0e1b4
Ayomide Oluniyi <ayorg360@gmail.com>
2024-12-24 03:13:29 +0200

    chore: remove user interfaces

--- src/app/i/_module/_interfaces/user.interface.ts
+++ src/app/i/_module/_interfaces/user.interface.ts
@@ -3,14 +3,3 @@ export interface IUserStatusUpdate {
   status: string;
   reason?: string;
 }
-
-interface IStatistics {
-  count: number;
-  percentageChange: number;
-}
-
-export interface IUserStatistics {
-  total: IStatistics;
-  subscribed: IStatistics;
-  suspended: Omit<IStatistics, 'percentageChange'>;
-}
